3381c46712ecbd9318a23446144c33f86e8411cd
alexandrurosca <alex.rosca1996@gmail.com>
2018-04-16 21:22:59 +0300

    second function

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,33 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(5);
+        //System.out.print(checkElementDivizibilWithList(list,10 ));
 
     }
 
-
-    boolean checkElementDivizibilWithList(ArrayList<Integer> list, int  elem){
-        for(Integer item: list){
-            if(elem % item != 0){
+    public  static boolean checkElementDivizibilWithList(ArrayList<Integer> list, int elem) {
+        for (Integer item : list) {
+            if (elem % item != 0) {
                 return false;
             }
         }
         return true;
     }
 
+    public static int calculateNewDivizor(int lastElemFromList, int current) {
+
+        for (int i = lastElemFromList + 1; i < Math.sqrt(current); i++) {
+            if (current % i == 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
 
 
 }
